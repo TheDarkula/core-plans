9ecfd1a72692df6ef06d33da9ffa576ca6461f67
Meade Kincke <thedarkula2049@gmail.com>
2018-12-25 06:04:17 +0000

    Updated atk to latest version.
    Signed-off-by: Meade Kincke <thedarkula2049@gmail.com>

--- atk/plan.sh
+++ atk/plan.sh
@@ -1,17 +1,19 @@
 pkg_name=atk
 pkg_origin=core
-pkg_version="2.27.1"
+pkg_version="2.30.0"
 pkg_description="Library for a set of interfaces providing accessibility. By supporting the ATK interfaces, an application or toolkit can be used with tools such as screen readers, magnifiers, and alternative input devices."
 pkg_upstream_url="https://developer.gnome.org/atk/"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('LGPL-2.0')
 pkg_source="https://download.gnome.org/sources/${pkg_name}/${pkg_version%.*}/${pkg_name}-${pkg_version}.tar.xz"
-pkg_shasum=673a953987b301ab1e24e7d11677b6e7ba3226411a168449ba946765b6d44297
+pkg_shasum=dd4d90d4217f2a0c1fee708a555596c2c19d26fef0952e1ead1938ab632c027b
 pkg_deps=(
   core/glib
   core/glibc
   core/libffi
   core/libiconv
+  core/meson
+  core/ninja
   core/pcre
 )
 pkg_build_deps=(
@@ -25,3 +27,9 @@ pkg_build_deps=(
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 pkg_pconfig_dirs=(lib/pkgconfig)
+
+do_build() {
+  attach
+  meson _build .
+  ninja -C _build dist
+}
