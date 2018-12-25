7ce3e9492d62ec68a7c3f18568b476ad74df1598
Meade Kincke <thedarkula2049@gmail.com>
2018-12-25 06:10:39 +0000

    Updated at-spi2-atk to latest version.
    Signed-off-by: Meade Kincke <thedarkula2049@gmail.com>

--- at-spi2-atk/plan.sh
+++ at-spi2-atk/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=at-spi2-atk
 pkg_origin=core
-pkg_version="2.26.1"
+pkg_version="2.30.0"
 pkg_description="Service Provider Interface for the Assistive Technologies available on the GNOME platform"
 pkg_upstream_url="https://wiki.linuxfoundation.org/accessibility/atk/at-spi/at-spi_on_d-bus"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('LGPL-2.0')
 pkg_source="https://download.gnome.org/sources/${pkg_name}/${pkg_version%.*}/${pkg_name}-${pkg_version}.tar.xz"
-pkg_shasum=b4f0c27b61dbffba7a5b5ba2ff88c8cee10ff8dac774fa5b79ce906853623b75
+pkg_shasum=e2e1571004ea7b105c969473ce455a95be4038fb2541471714aeb33a26da8a9a
 pkg_deps=(
   core/at-spi2-core
   core/atk
@@ -15,6 +15,7 @@ pkg_deps=(
   core/glibc
   core/libffi
   core/libiconv
+  core/meson
   core/pcre
 )
 pkg_build_deps=(
@@ -26,3 +27,7 @@ pkg_build_deps=(
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 pkg_pconfig_dirs=(lib/pkgconfig)
+
+do_build() {
+  attach
+}
